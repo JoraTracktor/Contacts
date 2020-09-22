a66ce009bdd226c9520fa148e540f60568c1abc2
Egor <georg300497@gmail.com>
2020-09-22 18:07:39 +0300

    Comm

--- Contacts/task/src/contacts/Main.java
+++ Contacts/task/src/contacts/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         ContactsBookManager contactsBook = ContactsBookManager.getInstance();
-        //contactsBook.readFromFile(args[0]);
-
         CommandLineMenu menu = new CommandLineMenu(contactsBook);
         menu.mainMenu();
     }
